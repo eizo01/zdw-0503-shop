c15207676e55833ef88de477128c2bb126b11199
zdw-nb <2399492494@qq,com>
2023-05-06 15:53:02 +0800

    order服务 - 定时关单mq配置策略

---
+++ zdw-order-service/src/main/java/com/zdw/order/controller/CallbackController.java
@@ -0,0 +1,26 @@
+package com.zdw.order.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: 曾德威
+ * @Date: 2023/5/6
+ * @Description: 欢迎访问我的个人博客:javazdw.top
+ */
+@Api("订单回调通知模块")
+@Controller
+@RequestMapping("/api/callback/order/v1")
+@Slf4j
+public class CallbackController {
+
+
+
+
+
+
+
+}
+
